ce5be13d27b3edb9116f0d0c85ea64757df55f2e
Roxaneryz <roxane5666@yahoo.gr>
2024-06-03 23:28:35 +0300

    searchBar

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,30 @@
+import toast, { Toaster } from "react-hot-toast";
+
+const SearchBar = ({ onSearch }) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const query = e.target.elements.searchimage.value.trim();
+    query === ""
+      ? toast.error("Oops! It looks like you forgot to enter a search term.")
+      : onSearch(query);
+    e.target.reset();
+  };
+  return (
+    <header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+        <button type="submit">Search</button>
+      </form>
+      <Toaster position="top-center" />
+    </header>
+  );
+};
+
+export default SearchBar;
+
+// const message = () => toast("Here is your toast.");
