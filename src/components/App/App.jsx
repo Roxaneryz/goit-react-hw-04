16e85b5a602b63dcdd7974146cdef8880d23e354
Roxaneryz <roxane5666@yahoo.gr>
2024-06-06 00:10:27 +0300

    imports of comp

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,23 @@
+import { useState, useRef, useEffect } from "react"
+
+import getImages from "../unsplash-api"
+import ErrorMessage    from "../ErrorMassage/ErrorMessage"
+import ImageModal from "../ImageModal/ImageModal"
+import Loader from "../Loader/Loader"
+import ImageGallery from "../ImageGallery/ImageGallery"
+import LoaderMore from "../LoaderMore/LoaderMore"
+import SearchBar from "../SearchBar/SearchBar"
 
 
 const App = () => {
+
+
+
+
+
+    
   return (
-    <div>App</div>
+    <div></div>
   )
 }
 
