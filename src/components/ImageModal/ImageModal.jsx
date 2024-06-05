e480c49fb36d7717d353c0cf9c48a8c9caab2655
Roxaneryz <roxane5666@yahoo.gr>
2024-06-05 23:32:35 +0300

    modal ok

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,9 +1,27 @@
+import Modal from "react-modal";
 
+import css from "./ImageModal.module.css";
 
-const ImageModal = () => {
+const ImageModal = ({ isOpen, onClose, image }) => {
   return (
-    <div>ImageModal</div>
-  )
-}
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      shouldCloseOnEsc={true}
+      className={css.modal}
+      overlayClassName={css.overlay}
+      ariaHideApp={false}
+    >
+      <div className={css.content} onClick={onClose}>
+        <img
+          src={image.urls.regular}
+          alt={image.alt_description || "Large view"}
+          className={css.image}
+        />
+      </div>
+    </Modal>
+  );
+};
+
 
 export default ImageModal
