d1e1eb55b777e063612cb32e898764e403824e5b
Roxaneryz <roxane5666@yahoo.gr>
2024-06-09 01:16:47 +0300

    finish the padding

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,5 @@
 import css from "./SearchBar.module.css"
-import { FcSearch } from "react-icons/fc";
+import { ImSearch } from "react-icons/im";
 
 import toast, { Toaster } from "react-hot-toast";
 
@@ -14,17 +14,20 @@ const SearchBar = ({ onSearch }) => {
   };
   return (
     <header className={css.search}>
-      <form onSubmit={handleSubmit}>
-        <input className={css.inputSearch}
-          name = "searchimage"
+      <form className={css.searchForm } onSubmit={handleSubmit}>
+        <input
+          className={css.inputSearch}
+          name="searchimage"
           type="text"
           autoComplete="off"
           autoFocus
           placeholder="Search images and photos"
         />
-        <button type="submit"><FcSearch/> Search</button>
+        <button type="submit">
+          <ImSearch /> Search
+        </button>
       </form>
-      <Toaster position="top-center" />
+      <Toaster position="top-center" reverseOrder={false} />
     </header>
   );
 };
